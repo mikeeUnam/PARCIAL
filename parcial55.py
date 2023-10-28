1e43bfdb505b1ccc757f5de42bf04cad87f77308
mikeeUnam <michaelmanriquebellido1234@gmail.com>
2023-10-27 19:41:07 -0500

    add all my files

---
+++ parcial55.py
@@ -0,0 +1,32 @@
+import time
+import random
+
+def multiplicacion():
+    resultado = random.randint(1,10) * random.randint(1,10)
+    print(f"Resultado de la multiplicacion {resultado}")
+
+def division():
+    resultado = random.randint(1,10) / random.randint(1,10)
+    print(f"Resultado de la division {resultado}")
+
+
+def suma():
+    resultado = random.randint(1,10) + random.randint(1,10)
+    print(f"Resultado de la suma {resultado}")
+
+
+def resta():
+    resultado = random.randint(1,10) - random.randint(1,10)
+    print(f"Resultado de la resta {resultado}")
+    
+operaciones = [resta, suma, multiplicacion, division, multiplicacion, suma, resta]
+
+contador = 0
+
+while contador < len(operaciones):
+    operador = operaciones[contador]
+    start_time = time.time()
+    while time.time() - start_time < 4:
+        operador()
+        time.sleep(1)
+    contador +=1
